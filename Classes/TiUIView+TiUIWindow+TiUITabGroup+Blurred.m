ca14bc942a797345419fc9cd479728bc97500ef2
Nikhil N <nikhil@dezinezync.com>
2013-10-29 13:28:28 +0530

    [ADD] Static Blur for tabGroup

--- Classes/TiUIView+TiUIWindow+TiUITabGroup+Blurred.m
+++ Classes/TiUIView+TiUIWindow+TiUITabGroup+Blurred.m
@@ -4,7 +4,7 @@
  * www.napp.dk
  */
 
-#import "TiUIView+TiUIWindow+Blurred.h"
+#import "TiUIView+TiUIWindow+TiUITabGroup+Blurred.h"
 
 @implementation TiUIView (WithShadow)
 
@@ -111,6 +111,88 @@
         [self.layer setShouldRasterize:YES];
     }
 }
+
+-(void)setStaticBlur_:(id)args
+{
+    //Doesn't work on iOS 6 and below.
+    if(![TiUtils isIOS7OrGreater])
+    {
+        return;
+    }
+    
+    ENSURE_SINGLE_ARG(args, NSDictionary);
+    
+    BOOL enabled = [[args valueForKey:@"enabled"] boolValue];
+    
+    if(!enabled)
+    {
+        //We no longer need the blurred image
+        [[self viewWithTag:100] removeFromSuperview];
+    }
+    else
+    {
+        
+        //If we have a UIImageView in our view, don't add a new one
+        if([self viewWithTag:100] == nil)
+        {
+            UIImageView *blurredImageView = [UIImageView new];
+            blurredImageView.tag=100;
+            blurredImageView.frame = self.frame;
+            
+            [self addSubview:blurredImageView];
+        }
+        
+        NSString *type = [args valueForKey:@"type"];
+        
+        // Create the image context
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, self.window.screen.scale);
+        
+        [self drawViewHierarchyInRect:self.frame afterScreenUpdates:NO];
+        
+        // Get the snapshot
+        UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+        
+        if(type != nil && type.length)
+        {
+            UIImage *blurredSnapshotImage;
+            
+            if([type isEqualToString:@"light"])
+            {
+                blurredSnapshotImage = [snapshotImage applyLightEffect];
+            }
+            else if([type isEqualToString:@"extra light"])
+            {
+                blurredSnapshotImage = [snapshotImage applyExtraLightEffect];
+            }
+            else if([type isEqualToString:@"dark"])
+            {
+                blurredSnapshotImage = [snapshotImage applyDarkEffect];
+            }
+            else if([type isEqualToString:@"tint"])
+            {
+                UIColor *tint = [[TiUtils colorValue:[args valueForKey:@"tint"]] _color];
+                blurredSnapshotImage = [snapshotImage applyTintEffectWithColor:tint];
+            }
+            
+            
+            ((UIImageView *)[self viewWithTag:100]).image = blurredSnapshotImage;
+            
+        }
+        else
+        {
+            ((UIImageView *)[self viewWithTag:100]).image = snapshotImage;
+        }
+        
+        // Be nice and clean your mess up
+        UIGraphicsEndImageContext();
+        
+    }
+    
+}
+
+@end
+
+@implementation TiUITabGroup (WindowWithShadow)
 
 -(void)setStaticBlur_:(id)args
 {
